aa9736e67fd605a79a456a7d8e29326ec2d12998
bvder92 <113997126+Bvder92@users.noreply.github.com>
2026-02-07 22:09:31 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
             <ul className="space-y-2 text-sm">
               <li>conciergerie.moraya@gmail.com</li>
               <li className="flex items-center justify-center md:justify-start gap-2">
-                <i class="fa-brands fa-whatsapp"></i>
+                <i class="fa-brands fa-whatsapp text-green-500 text-lg"></i>
                 <span><b>+33 6 20 81 51 30</b>   <b>+33 6 14 06 03 81</b></span>
               </li>
               <li className="flex items-center justify-center md:justify-start gap-2">
